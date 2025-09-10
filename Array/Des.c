0275073eda91c7815c12ce97ff10d52bc5f625e3
Rawlin <rawlinleitao08@gmail.com>
2025-09-10 18:14:54 +0530

    Ascending order

---
+++ Array/Des.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {4, 2, 32, 41, 99, 382, 121, 2332, 54, 3, 70};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                 for (int l = 0; l < size; l++) {
+                    printf("%d ", arr[l]);
+                }
+                printf("\n");
+            }
+        }
+    }
+    // printf("Array in descending order:\n");
+    // for (int i = 0; i < size; i++) {
+    //     printf("%d ", arr[i]);
+    // }
+
+    return 0;
+}
